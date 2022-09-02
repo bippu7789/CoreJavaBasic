3afad5ecb2c291abf7f4a96fc8f9d5f9e9dc9cfa
Bipin kumar <bipin0007mca@gmail.com>
2022-09-03 01:08:53 +0530

    Collection demo first commit.

---
+++ collection/src/collection/com/nt/CollectionDemo.java
@@ -0,0 +1,16 @@
+package collection.com.nt;
+
+public class CollectionDemo {
+
+	public static void main(String[] args) {
+		Object[] obj = new String[4];
+		obj[0] = "BIPIN";
+		obj[1] = "TEJA";
+		obj[2] = "GADE";
+		obj[3] = "Bishal";
+		for (int i = 0; i < obj.length; i++) {
+			System.out.println("Elements of arrays is ::" + obj[i]);
+		}
+	}
+
+}
