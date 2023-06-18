917e08fd14ce02fc260fa516ccc75f898a389e68
Bipin kumar <bipin0007mca@gmail.com>
2023-06-18 14:54:02 +0530

    print-string.

---
+++ StringHandling/src/workingWithString/StringDemo.java
@@ -0,0 +1,12 @@
+package workingWithString;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		char[] ch= {'a','b','c','d','e','f'};
+		String s6=new String(ch);
+		System.out.println("S6::"+s6);
+
+	}
+
+}
