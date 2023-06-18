130d498c527ad4c274227a18c66b85ccb0fb0804
Bipin kumar <bipin0007mca@gmail.com>
2023-06-18 15:19:20 +0530

    Count the number of cherector.

---
+++ StringHandling/src/workingWithString/StringTest.java
@@ -0,0 +1,13 @@
+package workingWithString;
+
+public class StringTest {
+
+	public static void main(String[] args) {
+		char[] ch= {'a','b','c','d','e','f'};
+		String s7=new String(ch,2,3);
+		System.out.println("s7::"+s7);
+		
+
+	}
+
+}
